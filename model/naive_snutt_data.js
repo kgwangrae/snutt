d046836129bcb4960b11e4c10ac419469ac8454a
kgwangrae <k.gwangrae@gmail.com>
2014-08-19 14:57:45 +0900

    Resolved iss16

--- model/naive_snutt_data.js
+++ model/naive_snutt_data.js
@@ -222,7 +222,7 @@ NaiveLectureModel.prototype = {
             return result;
         } else if (options.type === "course_number") {
             //교과목번호로 검색
-            var course_number = options.query_text.replace(/ /g, "").toLowerCase();
+            var course_number = safeString(options.query_text).replace(/ /g, "").toLowerCase();
             skip_count = 0;
             for (i = 0; i < lectures.length; i++){
                 var lecture_course_number = (safeString(lectures[i].course_number)+safeString(lectures[i].lecture_number)).replace(/ /g, "").toLowerCase();
